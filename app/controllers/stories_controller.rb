2aa1c79e95dd6e7bac3411c8d36150520f187019
mpanchuk <severynr@gmail.com>
2015-08-29 17:31:44 +0300

    CRUD for story

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,4 +1,5 @@
 class StoriesController < ApplicationController
+  before_action :find_story, only: [:update, :edit, :show, :destroy]
   
   def index
     @stories = Story.all
@@ -17,10 +18,35 @@ class StoriesController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @story.update(story_params)
+      render 'show'
+      flash[:notice] = "Story has been updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @story.destroy
+      redirect_to '/'
+    end
+  end
+
   private
 
   def story_params
     params.require(:story).permit(:body, :category_id)
+  end
+
+  def find_story
+    @story = Story.find(params[:id])
   end
 
 end
